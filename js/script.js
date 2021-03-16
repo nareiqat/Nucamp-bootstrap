1c247ba4624144c0d458595a5fde0deabdbbd89b
nareiqat <nabeelareiqat@gmail.com>
2021-03-16 19:17:12 -0400

    fixed some issues

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $(function() {
+
+    //selects the carousel dom node and calls the fucntion every 2 seconds
 $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+//add event click to carousel button
+    $("#carouselButton").on("click",function(){
+        //checks if the id has the specified child, 
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            //pause the carousel
             $(".carousel").carousel("pause");
+            //then remove the class 'fa-pause' to all the carousel button's children
             $("#carouselButton").children("i").removeClass("fa-pause");
+            // and then add a class back to those children called 'fa-play
             $("#carouselButton").children("i").addClass("fa-play");
         } else {
+            // call the carousel method on my carousel dom node element
             $(".carousel").carousel("cycle");
+            // same as previous block above just vice versa
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
